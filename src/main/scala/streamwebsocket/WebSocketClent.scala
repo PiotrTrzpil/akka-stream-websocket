a84d69a71f0aa82f8819bfb424ecd600056144e9
Piotr Trzpil <piotr.trzpil@gmail.com>
2015-01-26 08:44:15 +0000

    cleanup

--- src/main/scala/streamwebsocket/WebSocketClent.scala
+++ src/main/scala/streamwebsocket/WebSocketClent.scala
@@ -17,10 +17,8 @@ class APublisher extends ActorPublisher[Frame] {
    val receiveQueue = mutable.Queue[Frame]()
    def receive = {
       case f:Frame => receiveQueue.enqueue(f)
-         println("publisher client got Frame"+f)
          process()
       case Request(n) =>
-         println("publisher client got Request"+n)
          process()
 
       case Cancel =>
@@ -37,9 +35,7 @@ class ASubscriber(client:ActorRef) extends ActorSubscriber with ActorLogging{
       case ActorSubscriberMessage.OnError(ex) =>
          log.error("",ex)
       case ActorSubscriberMessage.OnComplete =>
-         log.info("on client COMPLETE")
       case ActorSubscriberMessage.OnNext(msg :Frame) =>
-         log.info("on client ONNEXT"+msg)
          client ! Send(msg)
    }
 
@@ -52,22 +48,16 @@ class Client(commander:ActorRef, val upgradeRequest: HttpRequest) extends websoc
 
    def businessLogic: Receive = {
       case websocket.UpgradedToWebSocket =>
-         log.info("on client websocket.UpgradedToWebSocket")
          publisher = context.actorOf(Props(classOf[APublisher]), "client-publisher")
          subscriber = context.actorOf(Props(classOf[ASubscriber], self),"client-subscriber")
          commander ! Websocket.Connection(ActorPublisher(publisher), ActorSubscriber(subscriber))
-      case Send(frame) => connection ! frame
-         log.info("on client Send(frame)")
+      case Send(frame) =>
+         connection ! frame
       case frame:Frame =>
          publisher ! frame
-         log.info("on client receive(frame)")
-      //val str = text.utf8String
-      //println("client received: "+str)
-      //probe.ref ! s"client received: "+str
 
       case _: Http.ConnectionClosed =>
          context.stop(self)
-      //  case x => connection ! x
    }
 
 }
